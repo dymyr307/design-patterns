ca5e4646e4ae50decadbd77e2e7df7b324c7d764
Volodymyr_D <dymyr307@gmail.com>
2024-03-05 23:09:24 +0200

    studying patterns

---
+++ Singleton/single.js
@@ -0,0 +1,33 @@
+class User {
+  constructor(login, email) {
+    this.login = login;
+    this.email = email;
+  }
+}
+
+class Editor extends User {
+  constructor(login, email) {
+    super(login, email);
+  }
+  createPost(title, text) {}
+}
+
+class Admin extends User {
+  static exist = false;
+  static instance = null;
+
+  constructor(login, email) {
+    if (Admin.exist) {
+      return;
+    }
+    super(login, email);
+    Admin.exist = true;
+    Admin.instance = this;
+  }
+}
+
+const admin1 = new Admin('admin', '');
+console.log(admin1);
+
+const admin2 = new Admin('Aleg', '');
+console.log(admin2);
