bb86883c72ab8a3ebff7ae5219310b2f5875c192
Volodymyr_D <dymyr307@gmail.com>
2024-03-10 22:15:18 +0200

    end facade

--- adapter/adapter.js
+++ adapter/adapter.js
@@ -1,6 +1,55 @@
-class Order {
-  constructor(order) {
-    this.order = order;
-    this.status = 'received';
+class User {
+  constructor(login, email) {
+    this.login = login;
+    this.email = email;
   }
 }
+
+class Editor extends User {
+  constructor(login, email) {
+    super(login, email);
+  }
+  createPost(title, text) {}
+}
+
+class Admin extends User {
+  static exist = false;
+  static instance = null;
+
+  constructor(login, email) {
+    if (Admin.exist) {
+      return;
+    }
+    super(login, email);
+    Admin.exist = true;
+    Admin.instance = this;
+  }
+}
+
+class UserCreator {
+  static userList = {
+    user: User,
+    editor: Editor,
+    admin: Admin,
+  };
+  static createUser(login, email, role = 'user') {
+    const Fabric = UserCreator.userList[role];
+
+    const instance = new Fabric(login, email);
+    instance.role = role;
+
+    return instance;
+  }
+}
+
+class UserAdapter {
+  static create(login, email, role = 'user') {
+    return UserCreator.createUser(login, email, role);
+  }
+}
+
+const user1 = UserAdapter.create('Bruce', 'b@mail.com');
+console.log(user1);
+
+const admin1 = UserAdapter.create('Bob', 'bob@mail.com', 'admin');
+console.log(admin1);
--- facade/facade.js
+++ facade/facade.js
@@ -1,55 +1,44 @@
-class User {
-  constructor(login, email) {
-    this.login = login;
-    this.email = email;
+class Order {
+  constructor(order) {
+    this.order = order;
+    this.status = 'received';
+
+    KitcenTask.createTask(this);
   }
 }
 
-class Editor extends User {
-  constructor(login, email) {
-    super(login, email);
+class KitcenTask {
+  constructor(task) {
+    this.task = task;
+    this.status = 'preparing';
   }
-  createPost(title, text) {}
-}
 
-class Admin extends User {
-  static exist = false;
-  static instance = null;
+  static createTask(task) {
+    const kTask = new KitcenTask(task);
 
-  constructor(login, email) {
-    if (Admin.exist) {
-      return;
-    }
-    super(login, email);
-    Admin.exist = true;
-    Admin.instance = this;
+    setTimeout(() => {
+      DeliveryTask.createTask(kTask);
+    }, 3000);
   }
 }
 
-class UserCreator {
-  static userList = {
-    user: User,
-    editor: Editor,
-    admin: Admin,
-  };
-  static createUser(login, email, role = 'user') {
-    const Fabric = UserCreator.userList[role];
+class DeliveryTask {
+  constructor(task) {
+    this.task = task;
+    task.task.status = 'in delivery';
+  }
 
-    const instance = new Fabric(login, email);
-    instance.role = role;
+  static createTask(task) {
+    const dTask = new DeliveryTask(task);
 
-    return instance;
+    setTimeout(() => {
+      dTask.task.status = 'complete';
+
+      console.log(dTask);
+    }, 3000);
   }
 }
 
-class UserAdapter {
-  static create(login, email, role = 'user') {
-    return UserCreator.createUser(login, email, role);
-  }
-}
+const o = new Order([{ id: 1, title: 'bear', price: 200, quantity: 5 }]);
 
-const user1 = UserAdapter.create('Bruce', 'b@mail.com');
-console.log(user1);
-
-const admin1 = UserAdapter.create('Bob', 'bob@mail.com', 'admin');
-console.log(admin1);
+console.log(o);
