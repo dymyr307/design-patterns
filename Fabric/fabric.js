f8d07f3f4eab222ac3a046ecd6e44d48f55eb508
Volodymyr_D <dymyr307@gmail.com>
2024-03-10 18:25:24 +0200

    fabric end

--- Fabric/fabric.js
+++ Fabric/fabric.js
@@ -1,1 +1,49 @@
+class User {
+  constructor(login, email) {
+    this.login = login;
+    this.email = email;
+  }
+}
 
+class Editor extends User {
+  constructor(login, email) {
+    super(login, email);
+  }
+  createPost(title, text) {}
+}
+
+class Admin extends User {
+  static exist = false;
+  static instance = null;
+
+  constructor(login, email) {
+    if (Admin.exist) {
+      return;
+    }
+    super(login, email);
+    Admin.exist = true;
+    Admin.instance = this;
+  }
+}
+
+class UserCreator {
+  static userList = {
+    user: User,
+    editor: Editor,
+    admin: Admin,
+  };
+  static create(login, email, role = 'user') {
+    const Fabric = UserCreator.userList[role];
+
+    const instance = new Fabric(login, email);
+    instance.role = role;
+
+    return instance;
+  }
+}
+
+const user1 = UserCreator.create('Bruce', 'b@mail.com');
+console.log(user1);
+
+const editor1 = UserCreator.create('Bob', 'bob@mail.com', 'editor');
+console.log(editor1);
